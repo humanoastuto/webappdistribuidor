466d8e21c5548d0afd97b2bf94a728893c3779b9
Ricardo <humanoastuto321@gmail.com>
2019-10-24 21:37:16 -0400

    google maps

--- webversion/src/app/app.module.ts
+++ webversion/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCGpohYz9t6hsj10jqD-4S5T0ohPpuVfSQ'
+    })],
   providers: [],
   bootstrap: [AppComponent]
 })
